d0efe1ddcc85eda9631421fd3167434658c55bf1
danish4994 <danish0ahmad@gmail.com>
2023-10-13 14:41:44 +0530

    "added form validation"

---
+++ script.js
@@ -0,0 +1,59 @@
+function validation(){
+    let username = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let phone = document.getElementById("mobile").value;
+    let subject = document.getElementById("subject").value;
+    let usernameStatus = false;
+    let emailStatus = false;
+    let PhoneStatus = false;
+    let subjectStatus = false;
+
+    if(username === ""){
+        document.getElementById("nameerror").innerHTML = "Please Enter Valid Name";
+    }
+    else{
+        document.getElementById("nameerror").innerHTML = "";
+        usernameStatus = true;
+    }
+
+    if(email === ""){
+        document.getElementById("emailerror").innerHTML = "Please Enter Valid Email";
+    }
+    else{
+        document.getElementById("emailerror").innerHTML = "";
+        emailStatus = true;
+    }
+
+    if(phone === ""){
+        document.getElementById("phoneerror").innerHTML = "Please Enter Valid phone Number";
+    }
+    else if(isNaN(phone)){
+        document.getElementById("phoneerror").innerHTML = "Please Enter Number Only";
+    } 
+    else if(phone.length<10){
+        document.getElementById("phoneerror").innerHTML = "please Enter 10 Digit" ;
+    }
+    else{
+        document.getElementById("phoneerror").innerHTML = "";
+        PhoneStatus = true;
+    }
+
+    if(subject === ""){
+        document.getElementById("subjecterror").innerHTML = "Please Enter valid Subject"
+    }
+    else{
+        document.getElementById("subjecterror").innerHTML = ""
+        subjectStatus = true;
+    }
+
+
+
+
+
+    if(usernameStatus === true && emailStatus === true && PhoneStatus === true && subjectStatus === true){
+        return true;
+    }  
+    else{
+        return false;
+    }
+}
